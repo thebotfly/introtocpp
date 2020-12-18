336f6b32351e7513fee0a96e5e392b4453caf529
thebotfly <69916612+thebotfly@users.noreply.github.com>
2020-12-18 11:40:40 -0600

    Added a new project might be last

--- Character.h
+++ Character.h
@@ -2,16 +2,7 @@
 class Character
 {
 public:
-	Character();
-	Character(float health, float damage);
-
-	void attack(Character* other);
-	float takeDamage(float damageAmount);
-
-	float getDamage() { return m_damage; }
-	float getHealth() { return m_health; }
-private:
-	float m_health;
-	float m_damage;
+	int health;
+	int damage;
 };
 
--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
-#include "Game.h"
+#include <fstream>
+#include "Character.h"
+
 int main() 
 {
-	Game game;
-	game.run();
+	Character player = Character();
+	player.health = 100;
+	player.damage = 5;
+
+	//Example of saving to a text flie.
+	std::fstream flie;
+	flie.open("save.txt", std::ios::out);
+	flie << player.health << std::endl;
+	flie << player.damage;
+	flie.close();
+
+
+	//Example of loading from a text flie.
+	Character player2 = Character();
+
+	flie.open("save.txt", std::ios::in);
+
+	if (flie.is_open()) 
+	{
+		return 1;
+	}
+	flie >> player2.health;
+	flie >> player2.damage;
+	flie.close();
+
+	std::cout << player2.health << std::endl;
+	std::cout << player2.damage << std::endl;
 	return 0;
 }
